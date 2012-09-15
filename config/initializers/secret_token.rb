e020cb28ac4cd514ff278c6b6765038007072573
Kaushik Thirthappa <kaushik@quadnode.com>
2012-09-15 18:24:37 +0530

    Kick-Off, Code On Pirates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeliveryBoy::Application.config.secret_token = 'd42663cbfb3ecb85a0fb2c2b35e7462dabfa94163d826d437470b904555b73c0be1a5aa6c2f509d0c05c74980af0d1b8e08fca7afa937e32741022610df3ec82'
