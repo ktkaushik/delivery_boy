fb98f480d42d3abbdb037fb5cc11353241c2b7b9
Kaushik Thirthappa <kaushik@quadnode.com>
2012-09-16 03:54:45 +0530

    api to send out response and processing done

--- app/controllers/api/deliveries_controller.rb
+++ app/controllers/api/deliveries_controller.rb
@@ -3,6 +3,16 @@ class Api::DeliveriesController < ApplicationController
   respond_to :json
 
   def create
-    @parsed_json = JSON.parse(params)
+    @delivery         = Delivery.find(params["transaction_id"])
+    @seller           = @delivery.seller
+    @courier_company  = @delivery.courier_company
+
+    # Updating attributes to mark it delivered !
+    if @delivery.update_attributes( delivered: true )
+      # Trigger to send mail
+      respond_with(status: :created, location: nil)
+    else
+      respond_with(status: :unprocessable_entity, location: nil)
+    end
   end
 end
