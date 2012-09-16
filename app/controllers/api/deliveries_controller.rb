a37442fa1faf838fe60c26b012b22b7bdd9de5e3
Kaushik Thirthappa <kaushik@quadnode.com>
2012-09-16 14:02:46 +0530

    trigger off emails as notifications for the seller and the courier company using, quite obviously, delayed_job_active_record

--- app/controllers/api/deliveries_controller.rb
+++ app/controllers/api/deliveries_controller.rb
@@ -6,10 +6,13 @@ class Api::DeliveriesController < ApplicationController
     @delivery         = Delivery.find(params["transaction_id"])
     @seller           = @delivery.seller
     @courier_company  = @delivery.courier_company
+    @delivery_person  = @delivery.delivery_person
 
     # Updating attributes to mark it delivered !
     if @delivery.update_attributes( delivered: true )
       # Trigger to send mail
+      NotifierMailer.delay.notify_seller( @seller, @courier_company, @delivery_person, @delivery)
+      NotifierMailer.delay.notify_courier_company( @seller, @courier_company, @delivery_person, @delivery)
       respond_with(status: :created, location: nil)
     else
       respond_with(@delivery.errors , status: :unprocessable_entity, location: nil)
