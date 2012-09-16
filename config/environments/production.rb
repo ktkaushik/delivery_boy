67b3632609a1022886a7084cf7c1148054292608
Kaushik Thirthappa <kaushik@quadnode.com>
2012-09-16 16:24:42 +0530

    added easy navigation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ DeliveryBoy::Application.configure do
 
   # Mailer Configurations
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'deliveryboy.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
